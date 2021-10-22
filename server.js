484c58f90650a1c0c0e736bb8b621f33e0b55851
Song-EunJu <jd06280@naver.com>
2021-10-22 19:05:02 +0900

    test redirct

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const nunjucks = require('nunjucks');
 const axios = require('axios');
 const qs = require('qs');
 const session = require('express-session');
-require('dotenv').config(); 
+require('dotenv').config(); // 환경설정 파일
+
+var id = 'sam'
+var pwd = 'sam1234'
+
+var mongoose = require("mongoose");
+mongoose.connect(`mongodb://${id}:${pwd}@13.209.66.117:27017/admin`);
 
 app.set('view engine','html');
 nunjucks.configure('views',{ // html 파일이 열리는 폴더를 view 폴더로 지정
@@ -33,28 +39,32 @@ app.get('/auth/kakao',(req,res)=>{
     const kakaoAuthURL = `https://kauth.kakao.com/oauth/authorize?client_id=${kakao.clientID}&redirect_uri=${kakao.redirectUri}&response_type=code&scope=profile_image`;
     res.redirect(kakaoAuthURL);
 })
+
+// 로그인 이후 나올 페이지 설정
  
 app.get('/auth/kakao/callback', async(req,res)=>{
     //axios>>promise object
-    try{//access토큰을 받기 위한 코드
-    token = await axios({//token
-        method: 'POST',
-        url: 'https://kauth.kakao.com/oauth/token',
-        headers:{
-            'content-type':'application/x-www-form-urlencoded'
-        },
-        data:qs.stringify({
-            grant_type: 'authorization_code',//특정 스트링
-            client_id:kakao.clientID,
-            client_secret:kakao.clientSecret,
-            redirectUri:kakao.redirectUri,
-            code:req.query.code,//결과값을 반환했다. 안됐다.
-        })//객체를 string 으로 변환
-    })
-}catch(err){
-    res.json(err.data);
-}
-//access토큰을 받아서 사용자 정보를 알기 위해 쓰는 코드
+
+    //access토큰을 받기 위한 코드 - 사용자 정보를 얻을 수 있음
+    try{
+        token = await axios({//token
+            method: 'POST',
+            url: 'https://kauth.kakao.com/oauth/token',
+            headers:{
+                'content-type':'application/x-www-form-urlencoded'
+            },
+            data:qs.stringify({
+                grant_type: 'authorization_code',//특정 스트링
+                client_id:kakao.clientID,
+                client_secret:kakao.clientSecret,
+                redirectUri:kakao.redirectUri,
+                code:req.query.code,//결과값을 반환했다. 안됐다.
+            })//객체를 string 으로 변환
+        })
+    }catch(err){
+        res.json(err.data);
+    }
+    //access토큰을 받아서 사용자 정보를 카카오에 요청
     let user;
     try{
         console.log(token);//access정보를 가지고 또 요청해야 정보를 가져올 수 있음.
@@ -72,13 +82,12 @@ app.get('/auth/kakao/callback', async(req,res)=>{
  
     req.session.kakao = user.data;
     
-    res.send('success');
     res.render('info',{
         profile_image,
     })
 })
  
- 
+// req.session.kakao 의 값은 항상 cookie에 저장되어 있으므로, 로그인 상태에서는 어느 페이지든 가져옴
 app.get('/auth/info',(req,res)=>{
     let {profile_image}=req.session.kakao.properties;
     res.render('info',{
@@ -91,6 +100,7 @@ app.get('/',(req,res)=>{
     res.render('index');
 });
  
+
 app.get(kakao.redirectUri)
  
 app.listen(8080, ()=>{
