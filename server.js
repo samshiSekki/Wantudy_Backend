dfe8c7cce35ef91790569cfe2aeff130321e82aa
ohyujeong <ohlife4@gmail.com>
2021-10-24 18:04:33 +0900

    stduy api test

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ const studyRouter = require("./routes/study");
 app.use('/study', studyRouter);
 
 //react bulid
+app.use(express.static(path.join(__dirname,'../build')))
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname,'../build/index.html'))
 })
