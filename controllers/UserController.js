4fc6a59bf63ae72f8bd2ed3375c62563e2aaafc5
Song-EunJu <jd06280@naver.com>
2021-10-27 13:23:42 +0900

    response data

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,14 +4,28 @@ const path=require('path');
 exports.saveUser = async function (req, res) {
     const { email, profileImage, accessToken } = req.body;
 
+    // 로그인 시 이미 등록된 유저인지 확인하고 토큰 교체 ( 토큰 교체는 처리 해줘야 함)
+    User.find({ email:email }, (err, users) => {
+        if(err)
+            return res
+                .status(500)
+                .json({ error: err })
+        else if(users){ // 이미 등록되어 있으면 해당 유저 정보를 갖고 메인으로 이동
+            return res
+                .status(200)
+                .json({users})
+        }    
+    });
+
     const user = new User({
         email,
         profileImage,
         accessToken
-    })
+    })    
 
     try {
-        await user.save();
+        await user.save(); // 등록되어있지 않은 유저인 경우 저장하고 닉네임 페이지로 이동
+
         return res
             .status(200)
             .json(user); // 위에서 담은 user 정보를 json형태로 보내면 const response에 들어감
@@ -22,31 +36,3 @@ exports.saveUser = async function (req, res) {
     }
 };
 
-exports.saveNickname = async function (req, res) {
-    const { email, nickname } = req.body;
-  
-    User.find({ email:email }, (err, users) => {
-        if(err)
-            return res
-                .status(500)
-                .json({ error: err })
-        console.log(res.json(users));
-    });
-
-    /*
-    if(email == 디비에 있고 디비에 nickname 값이 있는경우)
-      메세지 리턴 
-    else if email 디비에 있고 디비에 nickname 값 없는 경우 
-        닉네임 추가
-    else email 디비에 없는 경우 
-     에러메세지 리턴
-    */
-    // try {
-    //     await user.save();
-    //     return res
-    //         .status(200)
-    //         .json(user); // 위에서 담은 user 정보를 json형태로 보내면 const response에 들어감
-    // } catch (err) {
-    //     return res.status(500).json({ error: err })
-    // }
-};
