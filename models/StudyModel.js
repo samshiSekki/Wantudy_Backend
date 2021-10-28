9df6f88faabf5c0e53d4eed764da9482a8db9026
ohyujeong <ohlife4@gmail.com>
2021-10-28 22:36:23 +0900

    fetch : model array 유효성 검사

--- models/StudyModel.js
+++ models/StudyModel.js
@@ -4,11 +4,15 @@ const { Schema } = mongoose;
 const StduySchema = new Schema({
     // userId : {type: Schema.ObjectId, ref:"User", required: true},
     studyName: { type: String, required: true },
-    category: { type: [String], required: true },
+    category: {
+        type: [String],
+        required: true,
+        validate: v => Array.isArray(v) && v.length > 0,
+    },
     description: { type: String, required: true },
     onoff: { type: String, required: true },
     studyTime: { type: [String], required: true },
-    peopleNum: { type : Number, required: true },
+    peopleNum: { type: Number, required: true },
     requiredInfo: { type: [String], required: true },
     deadline: { type: Date, required: true },
 });
