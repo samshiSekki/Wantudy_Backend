5e8c6f59793d48ae56e9e46ee39262e6217c0e39
Song-EunJu <jd06280@naver.com>
2021-10-29 11:39:47 +0900

    swagger application api

--- models/Application.js
+++ models/Application.js
@@ -4,43 +4,38 @@ const { Schema } = mongoose;
 const applicationSchema = new Schema({
     name:{
         type:String,
-        required:true
     },
     gender:{
         type:String,
-        required:true
     },
     age:{
         type:Number,
-        required:true
     },
     school:{
         type:String,
-        required:true
     },
     major:{
         type:String,
-        required:true
     },
     attending:{
         type:String,
-        required:true
     },
     semester:{
-        type:[String]
+        type:[Number]
     },
     address:{
         type:String,
-        required:true
     },
     interests:{        
         type:[String],
-        required:true
     },
     keyword:{
         type:[String],
-        reqired:true     
+    },
+    applyMotive:{
+        type:String,
     }
+
 })
 
 module.exports = mongoose.model('Application', applicationSchema);
