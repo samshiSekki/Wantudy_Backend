0940fb25b071d70c7beacc17445bbd6267594360
Song-EunJu <jd06280@naver.com>
2021-10-29 14:43:17 +0900

    application Controller test

--- controllers/ApplicationController.js
+++ controllers/ApplicationController.js
@@ -5,7 +5,7 @@ const path=require('path');
 exports.saveApplication = async function (req, res) {
     const { name, gender, age, school, major, attending, semester, address, interests, keyword } = req.body;
 
-
+    
 
 
 
