13b9bdb07e9f0ee4ea5b247ca4a42cec55675468
Song-EunJu <jd06280@naver.com>
2021-11-18 04:05:10 +0900

    [feat] 과제 관련 DB 추가

--- models/Assignment.js
+++ models/Assignment.js
@@ -2,29 +2,37 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 var autoIncrement = require('mongoose-auto-increment');
 
+/* 과제 부여하는 경우 단순 과제 저장 */
+
 const assignmentSchema = new Schema({
     assignmentId: {
         type: Number,
         default:0
     },
-    userId: { // 과제 부여한 사람
+    userId: { // 과제 부여한  사람
         type: Number
     },
-    study: { // 어떤 스터디에 과제부여했는지
-        type: Schema.Types.ObjectId,
-        ref: 'StudyList',
+    // study: { // 어떤 스터디에 과제부여했는지
+    //     type: Schema.Types.ObjectId,
+    //     ref: 'StudyList',
+    // },
+    studyId: {
+        type:Number
     },
     assignmentName:{
         type:String,
         required:true
     },
-    description:{
-        type:String,
-        required:true
+    assignment:{
+        type:Buffer,
     },
     deadline:{
         type:Date,
         required:true
+    },
+    currentNum : { // 현재 제출한 인원
+        type:Number,
+        default:0
     }
 });
 
