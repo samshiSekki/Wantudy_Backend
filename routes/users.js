aa2382be3591af03b78f1ed34e203b22a3d3e5e1
ohyujeong <ohlife4@gmail.com>
2021-11-17 19:36:43 +0900

    [Feat] 일정 조율 API

--- routes/users.js
+++ routes/users.js
@@ -32,6 +32,10 @@ router.post('/:userId/opened-studylist/{studyId}/assignment', UserController.giv
 // 과제 관리 - 해야할 과제 조회 (내가 신청한 스터디에서 부여한 과제)
 router.get('/:userId/assignment', UserController.manageAssignment);
 
+//참여 스터디 일정 조율
+router.get('/:userId/ongoing-studylist/:studyId/schedule', UserController.schedule)
+router.post('/:userId/ongoing-studylist/:studyId/schedule', UserController.scheduleSave)
 
+router.get('/:userId/ongoing-studylist/:studyId/schedule-common', UserController.scheduleCommon)
 
 module.exports = router;
