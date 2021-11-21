5f5c096c5a82a5dd095338c86234cdd7dfea71cc
Song-EunJu <jd06280@naver.com>
2021-11-21 23:41:53 +0900

    [fix] 참여 스터디 조회 시 확정일정 추가

--- routes/users.js
+++ routes/users.js
@@ -53,13 +53,7 @@ router.put('/:userId/ongoing-studylist/:studyId/schedule', ScheduleController.sc
 //참여 스터디 공통 일정
 router.get('/:userId/ongoing-studylist/:studyId/schedule-common', ScheduleController.scheduleCommon)
 
-//참여 스터디 공통 일정 선택하기
+//참여 스터디 공통 일정 선택하기 _ 최종 선택
 router.put('/:userId/ongoing-studylist/:studyId/schedule-common', ScheduleController.scheduleCommonLast)
-
-// // 과제 테스트
-// router.post('/uploadFileWithOriginalFilename', uploadWithOriginalFilename.single('attachment'), function(req,res){ // 5
-//     res.render('confirmation', { file:req.file, files:null });
-// });
-
 
 module.exports = router;
